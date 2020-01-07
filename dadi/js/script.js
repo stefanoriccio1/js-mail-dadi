1671a4e0838b3749e96e305f6f8c5a5ee4b4f2f2
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-07 17:15:59 +0100

    Richiesto prompt per iniziare il gioco

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,9 @@
 alert('ciao')
+
+// Richiedere agli utenti un numero da 1 a 10, chi fa il più alto vince
+
+var giocatore1 = prompt('Per partecipare scrivi "Sì"');
+console.log (Math.floor(Math.random() * 6) +1);
+
+var giocatore2 = prompt('per sfidare l\'avversario scrivi "Accetto"');
+console.log (Math.floor(Math.random() * 6) +1);
