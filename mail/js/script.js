1bb0c567e55af825aebf6d56f01b81a665bfc3fb
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-07 16:03:50 +0100

    Inserito alert in caso di email presente, tentativo di segnalare errore in caso contrario

--- mail/js/script.js
+++ mail/js/script.js
@@ -9,7 +9,15 @@ console.log(emailUtente);
 var listaEmail = ['stefano@tiscali.it', 'andrea@tiscali.it', 'giovanni@tiscali.it', 'lucia@tiscali.it', 'marco@tiscali.it', 'matteo@tiscali.it', 'giacomo@tiscali.it'];
 // console.log(listaEmail);
 for (var i = 0; i < listaEmail.length; i++) {
-console.log(listaEmail[i]);
+ console.log(listaEmail[i]);
+
+ if (emailUtente == listaEmail[i]){
+  alert('Grazie! Ora puoi accedere e navigare il nostro sito');
+ }
+ else {
+   console.log('errore');
+ }
 }
+
 
 // Se la mail è presente dargli un messaggio di benvenuto
