0a476cf623ee947e38fbfce242f9d083f2cedcf0
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-07 16:56:16 +0100

    Corretto, entrambi gli alert funzionano

--- mail/js/script.js
+++ mail/js/script.js
@@ -8,15 +8,21 @@ var emailUtente = prompt('Per accedere devi verificare la tua email');
 
 var listaEmail = ['stefano@tiscali.it', 'andrea@tiscali.it', 'giovanni@tiscali.it', 'lucia@tiscali.it', 'marco@tiscali.it', 'matteo@tiscali.it', 'giacomo@tiscali.it'];
 // console.log(listaEmail);
-var nonTrovata = alert ('email non trovata');
+var nonTrovata = false;
 for (var i = 0; i < listaEmail.length; i++) {
  // console.log(listaEmail[i]);
  var emailVerifica = listaEmail[i];
 
  if (emailUtente == emailVerifica){
-  nonTrovata = alert('Grazie! Ora puoi accedere e navigare il nostro sito');
+  nonTrovata = true;
   // console.log(emailUtente)
  }
+}
+if (nonTrovata == false){
+  alert ('Spiacenti, email non trovata');
+}
+else {
+  alert ('Grazie! Ora puoi navigare il nostro sito');
 }
 
 
