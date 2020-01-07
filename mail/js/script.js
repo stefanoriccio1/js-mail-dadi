adf2fffc2af52ef7ba35929d52ee4efd9edf59f8
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-07 15:30:14 +0100

    impostato ciclo for

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,6 +7,9 @@ console.log(emailUtente);
 // controlare che la sua mail sia nella lista che abbiamo
 
 var listaEmail = ['stefano@tiscali.it', 'andrea@tiscali.it', 'giovanni@tiscali.it', 'lucia@tiscali.it', 'marco@tiscali.it', 'matteo@tiscali.it', 'giacomo@tiscali.it'];
-console.log(listaEmail);
+// console.log(listaEmail);
+for (var i = 0; i < listaEmail.length; i++) {
+console.log(listaEmail[i]);
+}
 
 // Se la mail è presente dargli un messaggio di benvenuto
