b4af1063518a6a7a7ec734cd4761de0345012f04
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-07 16:59:35 +0100

    Pulito codice js

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,21 +1,14 @@
-// alert ('ciao');
-
-// chiedere all'utente la sua email
-
 var emailUtente = prompt('Per accedere devi verificare la tua email');
-// console.log(emailUtente);
-// controlare che la sua mail sia nella lista che abbiamo
 
 var listaEmail = ['stefano@tiscali.it', 'andrea@tiscali.it', 'giovanni@tiscali.it', 'lucia@tiscali.it', 'marco@tiscali.it', 'matteo@tiscali.it', 'giacomo@tiscali.it'];
-// console.log(listaEmail);
+
 var nonTrovata = false;
 for (var i = 0; i < listaEmail.length; i++) {
- // console.log(listaEmail[i]);
+
  var emailVerifica = listaEmail[i];
 
  if (emailUtente == emailVerifica){
   nonTrovata = true;
-  // console.log(emailUtente)
  }
 }
 if (nonTrovata == false){
@@ -24,6 +17,3 @@ if (nonTrovata == false){
 else {
   alert ('Grazie! Ora puoi navigare il nostro sito');
 }
-
-
-// Se la mail è presente dargli un messaggio di benvenuto
