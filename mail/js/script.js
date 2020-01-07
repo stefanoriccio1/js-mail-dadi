b1b0a7ec90cd5f4308b171b38f420c7fab08a3fc
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-07 15:22:14 +0100

    Richiesta email all'utente

---
+++ mail/js/script.js
@@ -0,0 +1,9 @@
+alert ('ciao');
+
+// chiedere all'utente la sua email
+
+var emailUtente = prompt('Per accedere devi verificare la tua email');
+console.log(emailUtente);
+// controlare che la sua mail sia nella lista che abbiamo
+
+// Se la mail è presente dargli un messaggio di benvenuto
