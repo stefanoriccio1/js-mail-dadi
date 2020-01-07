82aea8ffb633e92c6b36cab0083ab97abd19827e
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-07 15:26:05 +0100

    inserita lista email, la console visualizza correttamente

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,9 +1,12 @@
-alert ('ciao');
+// alert ('ciao');
 
 // chiedere all'utente la sua email
 
 var emailUtente = prompt('Per accedere devi verificare la tua email');
 console.log(emailUtente);
 // controlare che la sua mail sia nella lista che abbiamo
+
+var listaEmail = ['stefano@tiscali.it', 'andrea@tiscali.it', 'giovanni@tiscali.it', 'lucia@tiscali.it', 'marco@tiscali.it', 'matteo@tiscali.it', 'giacomo@tiscali.it'];
+console.log(listaEmail);
 
 // Se la mail è presente dargli un messaggio di benvenuto
