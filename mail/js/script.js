7d37c0ec152cadb7e012174654d2d45100a68a26
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-07 16:33:20 +0100

    Secondo tentativo di messaggio "accesso negato"

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,19 +3,19 @@
 // chiedere all'utente la sua email
 
 var emailUtente = prompt('Per accedere devi verificare la tua email');
-console.log(emailUtente);
+// console.log(emailUtente);
 // controlare che la sua mail sia nella lista che abbiamo
 
 var listaEmail = ['stefano@tiscali.it', 'andrea@tiscali.it', 'giovanni@tiscali.it', 'lucia@tiscali.it', 'marco@tiscali.it', 'matteo@tiscali.it', 'giacomo@tiscali.it'];
 // console.log(listaEmail);
+var nonTrovata = alert ('email non trovata')
 for (var i = 0; i < listaEmail.length; i++) {
- console.log(listaEmail[i]);
+ // console.log(listaEmail[i]);
+ var emailVerifica = listaEmail[i];
 
- if (emailUtente == listaEmail[i]){
-  alert('Grazie! Ora puoi accedere e navigare il nostro sito');
- }
- else {
-   console.log('errore');
+ if (emailUtente == emailVerifica){
+  nonTrovata = alert('Grazie! Ora puoi accedere e navigare il nostro sito');
+  // console.log(emailUtente)
  }
 }
 
